454bbcbbed2b777d484c4cae28e2e952f42893b7
bredcat999 <116664249+bredcat999@users.noreply.github.com>
2023-04-16 18:00:13 -0500

    Colors in the terminal

--- main.cpp
+++ main.cpp
@@ -1,79 +1,66 @@
-#include "include.h" //arguable easier
+#include "termcolors.h"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
 
-// Tibe file classe
-class TibeFile
-{
+class Shell {
 public:
-	void load()
-	{
-		std::cout << "Loading .tibe archive...";
-	}
+  void help() {
+    std::cout << BLUE << "TIMEBOM CONSOLE\n" << RESET;
+    std::cout << "Commands:\n" << YELLOW;
+    std::cout << "  help\n";
+    std::cout << "  exit\n";
+    std::cout << "  load\n";
+    std::cout << RESET;
+  }
+  // Tibe file class
+};
 
+class TibeFile {
 public:
-	void save()
-	{
-		std::cout << "Saving .tibe archive...";
-		// We'll probably need to find something to handle archives like .zip
-	}
-
-public:
-	void tibeMetaData()
-	{ // I thought this could come in handy.
-		std::cout << "Listing tibe metadata\n";
-	}
+  void loadTibeFile() {
+    std::cout << BOLDMAGENTA << "Loading .tibe archive...\n" << RESET;
+  }
 };
 
 // Settings for the console and engine
-class Settings
-{
+class Settings {
 public:
-	class Screen
-	{
-	public:
-		int frameRate = 60;
-		int screenWidth = 1200;
-		int screenHeight = 800;
-	};
+  class Screen {
+  public:
+    int frameRate = 60;
+    int screenWidth = 1200;
+    int screenHeight = 800;
+  };
 
-	class Sound
-	{
-		int volume = 100;
-	};
+  class Sound {
+    int volume = 100;
+  };
 
-	class TibeFiles
-	{
-		std::string tibeDir;
-		std::string tibeName;
-	};
+  class TibeFiles {
+    std::string tibeDir;
+    std::string tibeName;
+  };
 };
-
-// Take and return console input
-std::string takeConsoleInput()
-{
-	std::string input;
-	std::cout << "\n> ";
-	getline(std::cin, input);
-	return input;
-}
 
 // Run a command from function parameter
 TibeFile tibe;
-void runCommand(std::string c)
-{
-	if (c == "load")
-	{
-		tibe.load();
-	}
-	else if (c == "save")
-	{
-		tibe.save();
-	}
-}
+Settings consoleSettings;
+Shell shell;
 
-int main()
-{
-	bool conti = true; // easier to change
-	std::cout << "TimeBom Console\n";
-	while (conti)
-		runCommand(takeConsoleInput());
+int main() {
+  bool shellIsNotClosed = true; // I really like verbose-ish naming schemes
+  std::cout << "TimeBom Console\n";
+  while (shellIsNotClosed) {
+    std::cout << BLUE << "> " << RESET;
+    std::string command;
+    std::getline(std::cin, command);
+    if (command == "exit") {
+      shellIsNotClosed = false;
+    } else if (command == "help") {
+      shell.help();
+    } else if (command == "load") {
+      tibe.loadTibeFile();
+    }
+  }
 }
